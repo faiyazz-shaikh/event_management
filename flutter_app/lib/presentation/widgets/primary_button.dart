38c0ba9626788b542299cec3fb9eb29b5b0a2437
faiyaz <55284898+faiyazz-shaikh@users.noreply.github.com>
2023-12-19 08:32:32 +0530

    ✨Initial flutter setup (#10)

---
+++ flutter_app/lib/presentation/widgets/primary_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  const PrimaryButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Container(
+        margin: const EdgeInsets.all(30),
+        color: Colors.pink,
+        child: Column(
+          children: [
+          ],
+        ),
+      ),
+    );
+  }
+}
