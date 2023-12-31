e0022364c1bcccb01b723104cc72843ab3890573
faiyaz <faiyazshaikh7351@gmail.com>
2023-12-31 16:14:05 +0530

    ✨On boarding page

---
+++ flutter_app/lib/config/themes/app_theme.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/core/resources/app_colors.dart';
+import 'package:flutter_app/core/resources/app_text_style.dart';
+import 'package:flutter_app/core/resources/font_manager.dart';
+
+class AppTheme {
+  static ThemeData get light {
+    return ThemeData(
+      primaryColor: AppColors.color000000,
+      primaryColorLight: AppColors.lightGrey,
+      primarySwatch: Colors.grey,
+      hintColor: AppColors.lowOpacityGrey,
+      shadowColor: AppColors.veryLowOpacityGrey,
+      focusColor: AppColors.black,
+      disabledColor: AppColors.black54,
+      dialogBackgroundColor: AppColors.black87,
+      hoverColor: AppColors.black45,
+      indicatorColor: AppColors.black38,
+      dividerColor: AppColors.black12,
+      cardColor: AppColors.lightBlack,
+      scaffoldBackgroundColor: AppColors.white,
+      iconTheme: const IconThemeData(color: AppColors.black38),
+      chipTheme:
+          const ChipThemeData(backgroundColor: AppColors.veryLowOpacityGrey),
+      highlightColor: AppColors.lowOpacityGrey,
+      canvasColor: AppColors.transparent,
+      splashColor: AppColors.white,
+      appBarTheme: AppBarTheme(
+        elevation: 0,
+        color: AppColors.colorFFFFFF,
+        shadowColor: AppColors.lowOpacityGrey,
+        iconTheme: const IconThemeData(color: AppColors.colorFFFFFF),
+        titleTextStyle:
+            getNormalStyle(fontSize: FontSize.s16, color: AppColors.black),
+      ),
+      textTheme: TextTheme(
+        bodyLarge: getNormalStyle(color: AppColors.black),
+        bodyMedium: getNormalStyle(color: AppColors.imageGrey),
+        bodySmall: getNormalStyle(color: AppColors.grey),
+        displayLarge: getNormalStyle(color: AppColors.grey, fontSize: 15),
+        displayMedium: getBoldStyle(color: AppColors.black, fontSize: 15),
+        displaySmall: getMediumStyle(color: AppColors.black, fontSize: 15),
+        headlineSmall: getNormalStyle(color: AppColors.shimmerLightGrey),
+        titleLarge: getNormalStyle(color: Colors.white),
+        titleSmall: getNormalStyle(color: AppColors.darkWhite),
+        titleMedium: getNormalStyle(color: AppColors.lightGrey),
+      ),
+      bottomAppBarTheme: const BottomAppBarTheme(color: AppColors.black26),
+    );
+  }
+
+  static ThemeData get dark {
+    return ThemeData(
+      primaryColor: AppColors.black,
+      primaryColorLight: AppColors.black54,
+      primarySwatch: Colors.grey,
+      hintColor: AppColors.darkGray,
+      shadowColor: AppColors.darkGray,
+      focusColor: AppColors.white,
+      dialogBackgroundColor: AppColors.white,
+      highlightColor: AppColors.grey,
+      hoverColor: AppColors.grey,
+      indicatorColor: AppColors.grey,
+      dividerColor: AppColors.grey,
+      iconTheme: const IconThemeData(color: AppColors.white),
+      chipTheme: const ChipThemeData(backgroundColor: AppColors.lightDarkGray),
+      cardColor: AppColors.darkGray,
+      disabledColor: AppColors.white,
+      scaffoldBackgroundColor: AppColors.black,
+      canvasColor: AppColors.transparent,
+      splashColor: AppColors.darkGray,
+      appBarTheme: AppBarTheme(
+        elevation: 0,
+        iconTheme: const IconThemeData(color: AppColors.white),
+        color: AppColors.black,
+        shadowColor: AppColors.lowOpacityGrey,
+        titleTextStyle:
+            getNormalStyle(fontSize: FontSize.s16, color: AppColors.white),
+      ),
+      textTheme: TextTheme(
+        bodyLarge: getNormalStyle(color: AppColors.white),
+        bodyMedium: getNormalStyle(color: AppColors.darkGray),
+        bodySmall: getNormalStyle(color: AppColors.lightGrey),
+        displayLarge: getNormalStyle(color: AppColors.grey, fontSize: 15),
+        displayMedium: getBoldStyle(color: AppColors.white, fontSize: 15),
+        displaySmall: getMediumStyle(color: AppColors.white, fontSize: 15),
+        headlineSmall: getNormalStyle(color: Colors.grey[500]!),
+        titleLarge: getNormalStyle(color: AppColors.shimmerDarkGrey),
+        titleSmall: getNormalStyle(color: AppColors.darkGray),
+        titleMedium: getNormalStyle(color: AppColors.darkGray),
+      ),
+      bottomAppBarTheme: const BottomAppBarTheme(color: AppColors.grey),
+    );
+  }
+}
--- flutter_app/lib/presentation/pages/register/onboarding.dart
+++ flutter_app/lib/presentation/pages/register/onboarding.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/core/helper.dart';
+import 'package:flutter_app/core/resources/app_colors.dart';
+import 'package:flutter_app/presentation/widgets/primary_button.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -10,14 +13,37 @@ class OnboardingPage extends StatefulWidget {
 class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(10),
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: SafeArea(
         child: Container(
-          margin: const EdgeInsets.all(30),
-          color: Colors.pink,
+          margin: const EdgeInsets.fromLTRB(18, 0, 18, 15),
+          padding: const EdgeInsets.all(15),
+          decoration: const BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
+            boxShadow: [
+              BoxShadow(
+                color: AppColors.transparentGrey,
+                blurRadius: 15,
+              ),
+            ],
+          ),
           child: Column(
             children: [
+              const Spacer(),
+              PrimaryButton(
+                onPressed: () {},
+                title: getAppLocal.logIn,
+                showBorder: true,
+                backGroundColor: AppColors.white,
+                textColor: AppColors.color46BCC3,
+              ),
+              const SizedBox(height: 10),
+              PrimaryButton(
+                onPressed: () {},
+                title: getAppLocal.signUp,
+              ),
             ],
           ),
         ),
